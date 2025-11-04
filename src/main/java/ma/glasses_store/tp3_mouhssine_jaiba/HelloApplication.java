6839879982505c828e5de731e9838630ba6bee57
Jaiba Mouhssine <Mouhssine.Jaiba@emsi-edu.ma>
2025-11-04 23:06:22 +0100

    intialisation

---
+++ src/main/java/ma/glasses_store/tp3_mouhssine_jaiba/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.glasses_store.tp3_mouhssine_jaiba;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ma/glasses_store/tp3_mouhssine_jaiba/ressource/GuideTouristiquResource.java
@@ -0,0 +1,8 @@
+package ma.glasses_store.tp3_mouhssine_jaiba.ressource;
+
+/**
+ * @author SETUP GAME
+ **/
+public class GuideTouristiquResource {
+
+}
